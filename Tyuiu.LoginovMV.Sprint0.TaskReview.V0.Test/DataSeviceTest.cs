53d8787f1cc8ed494b0d14090a687c4ec1fd791d
Максим Логинов <maxdompc@gmail.com>
2023-09-26 17:59:46 +0500

    Add project files.

---
+++ Tyuiu.LoginovMV.Sprint0.TaskReview.V0.Test/DataSeviceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.LoginovMV.Sprint0.TaskReview.V0.Lib;
+
+namespace Tyuiu.LoginovMV.Sprint0.TaskReview.V0.Test
+{
+    [TestClass]
+    public class DataSeviceTest
+    {
+        [TestMethod]
+        public void CheckCalc()
+        {
+            DataService ds = new DataService();
+            int x = 1;
+            int y = 1;
+            int z = 1;
+            var res = ds.Calc(x, y, z);
+            Assert.AreEqual(15, res);
+        }
+    }
+}
---
+++ Tyuiu.LoginovMV.Sprint0.TaskReview.V0/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.LoginovMV.Sprint0.TaskReview.V0.Lib;
+
+namespace Tyuiu.LoginovMV.Sprint0.TaskReview.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            int x;
+            Console.WriteLine("Введите переменную х:");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            int y;
+            Console.WriteLine("Введите переменную y:");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            int z;
+            Console.WriteLine("Введите переменную z:");
+            z = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("*****************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                *");
+            Console.WriteLine("*****************************");
+
+            Console.WriteLine(ds.Calc(x, y, z));
+            Console.ReadKey();
+        }
+    }
+}
